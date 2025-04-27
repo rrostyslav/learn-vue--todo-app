c82887bb7da1ecc66e84d6ed90fa1796f6fa3ec6
Rostyslav Rachynskyi <r.rostik2000@gmail.com>
2025-04-27 03:27:48 +0300

    chore: base feature sliced design setup

--- src/app/App.vue
+++ src/app/App.vue
@@ -3,7 +3,7 @@
 </script>
 
 <template>
-  <h1>Hello</h1>
+  <h1 class="bg-blue-500 flex justify-center items-center h-screen text-white text-7xl">Hello</h1>
 
   <!-- <RouterView /> -->
 </template>
